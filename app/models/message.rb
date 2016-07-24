7a6bb1e8a7124ef5ec3959dedc8644a2432f105b
snowpooll <snowpoollovely@gmail.com>
2016-07-24 13:48:20 +0000

    age cloumn add

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ActiveRecord::Base
     validates :name, length:{maximum: 20}, presence: true
     #内容は必須かつ２文字以上３０文字以内
     validates :body, length: {minimum: 2,maximum: 30},presence: true
+    #数値入力必須かつ0以上１００未満
+    validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than: 100 }, presence: true
 end
